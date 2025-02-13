02e4d9c97f238b06bef93753f1bdfbc35c8ba04f
thiziribrahimi <thiziribrahimi092@gmail.com>
2025-02-13 22:19:37 +0100

    v2 ajouter supprimer et modifier

--- src/app/controllers/weight_entries_controller.rb
+++ src/app/controllers/weight_entries_controller.rb
@@ -1,5 +1,49 @@
 class WeightEntriesController < ApplicationController
+  before_action :set_weight_entry, only: [:show, :edit, :update, :destroy]
+
   def index
     @weight_entries = WeightEntry.order(date: :desc)
+  end
+
+  def new
+    @weight_entry = WeightEntry.new
+  end
+
+  def create
+    @weight_entry = WeightEntry.new(weight_entry_params)
+    if @weight_entry.save
+      redirect_to weight_entries_path, notice: "Entrée de poids ajoutée avec succès."
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @weight_entry.update(weight_entry_params)
+      redirect_to weight_entries_path, notice: "Entrée de poids mise à jour."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @weight_entry.destroy
+    redirect_to weight_entries_path, notice: "Entrée supprimée."
+  end
+
+  private
+
+  def set_weight_entry
+    @weight_entry = WeightEntry.find(params[:id])
+  end
+
+  def weight_entry_params
+    params.require(:weight_entry).permit(:weight, :date)
   end
 end
--- src/app/models/weight_entry.rb
+++ src/app/models/weight_entry.rb
@@ -1,2 +1,5 @@
+# app/models/weight_entry.rb
 class WeightEntry < ApplicationRecord
-end
+    validates :weight, presence: true, numericality: { greater_than: 0 }
+    validates :date, presence: true
+  end
