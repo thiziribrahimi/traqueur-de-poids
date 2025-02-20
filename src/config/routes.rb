9f779d6166b4393c30e6619e80bc49d5814533e7
thiziribrahimi <thiziribrahimi092@gmail.com>
2025-02-20 13:03:00 +0100

    login with google

--- src/config/routes.rb
+++ src/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
+  controllers: {
+      omniauth_callbacks: 'omniauth_callbacks'
+    }
   get "weight_entries/index"
   root "weight_entries#index"
   resources :weight_entries
